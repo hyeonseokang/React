6206aae00b0f0b0b150d0e4e10daeade26dd2139
hyunseo <hansx2079@naver.com>
2021-11-07 23:49:28 +0900

    1

--- reduxtest/src/index.js
+++ reduxtest/src/index.js
@@ -2,17 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux';
+import rootReducer from './modules';
+
 import reportWebVitals from './reportWebVitals';
-import './exercise';
+const store = createStore(rootReducer); // 스토어를 만듭니다.
+console.log(store.getState()); // 스토어의 상태를 확인해봅시다.
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+ReactDOM.render(<App />, document.getElementById('root'));
 reportWebVitals();
