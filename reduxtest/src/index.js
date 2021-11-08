836e255bb90aed7864fc1d3d62e1f1cb1c35d83f
hyunseo <hansx2079@naver.com>
2021-11-08 23:07:09 +0900

    redux

--- reduxtest/src/index.js
+++ reduxtest/src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import rootReducer from './modules';
+import { composeWithDevTools } from 'redux-devtools-extension'; // 리덕스 개발자 도구
 
 import reportWebVitals from './reportWebVitals';
-const store = createStore(rootReducer); // 스토어를 만듭니다.
+const store = createStore(rootReducer, composeWithDevTools()); // 스토어를 만듭니다.
 console.log(store.getState()); // 스토어의 상태를 확인해봅시다.
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
+  );
 reportWebVitals();
