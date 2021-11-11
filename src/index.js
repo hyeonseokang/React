ba0b87f666851e560fe5167e23e7366ed09e0ebd
hyunseo <hansx2079@naver.com>
2021-11-11 22:28:25 +0900

    2

--- src/index.js
+++ src/index.js
@@ -9,7 +9,9 @@ import rootReducer from './moduels';
 import { composeWithDevTools } from 'redux-devtools-extension'; // 리덕스 개발자 도구
 import myLogger from './middlewares/myLogger';
 import logger from 'redux-logger';
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)));
+import ReduxThunk from 'redux-thunk';
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(ReduxThunk, logger)));
 
 ReactDOM.render(
   <Provider store={store}>
