58328b1e4e40f415c700bfa970acb02ad1c4f51b
hyunseo <hansx2079@naver.com>
2021-11-15 12:07:33 +0900

    redux saga

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import PostListPage from './pages/PostListPage';
 import PostPage from './pages/PostPage';
+import CounterContainer from './containers/CounterContainer';
 
 function App() {
   return (
     <>
+      <CounterContainer />
       <Route path="/" component={PostListPage} exact={true} />
       <Route path="/:id" component={PostPage} />
     </>
--- src/index.js
+++ src/index.js
@@ -1,23 +1,38 @@
-import reportWebVitals from './reportWebVitals';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import rootReducer from './moduels';
-import { composeWithDevTools } from 'redux-devtools-extension'; // 리덕스 개발자 도구
-import myLogger from './middlewares/myLogger';
+import rootReducer, { rootSaga } from './moduels';
 import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import ReduxThunk from 'redux-thunk';
-import { BrowserRouter, Router } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
+import createSagaMiddleware from 'redux-saga';
 
 const customHistory = createBrowserHistory();
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(ReduxThunk.withExtraArgument({history:customHistory}), logger)));
+const sagaMiddleware = createSagaMiddleware(); // 사가 미들웨어를 만듭니다.
+
+const store = createStore(
+  rootReducer,
+  // logger 를 사용하는 경우, logger가 가장 마지막에 와야합니다.
+  composeWithDevTools(
+    applyMiddleware(
+      ReduxThunk.withExtraArgument({ history: customHistory }),
+      sagaMiddleware, // 사가 미들웨어를 적용하고
+      logger
+    )
+  )
+); // 여러개의 미들웨어를 적용 할 수 있습니다.
+
+sagaMiddleware.run(rootSaga); // 루트 사가를 실행해줍니다.
+// 주의: 스토어 생성이 된 다음에 위 코드를 실행해야합니다.
 
 ReactDOM.render(
-  <Router history = {customHistory}>
+  <Router history={customHistory}>
     <Provider store={store}>
       <App />
     </Provider>
